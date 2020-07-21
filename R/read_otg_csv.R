eb903286c20a389cd12f7d1902ca834dd76aa5ef
Mike Ackerman <dirtpathwanderer@gmail.com>
2020-07-21 17:21:16 -0600

    finished read_otg_csv() draft

--- R/read_otg_csv.R
+++ R/read_otg_csv.R
@@ -6,13 +6,12 @@
 #' @author Mike Ackerman and Kevin See
 #'
 #' @inheritParams get_file_nms
-#'
-#' @param otg_type what type of on-the-ground (otg) data would the user like to read? As of the iniitation of the
-#' function a character vector of the file name e.g., "surveyPoint_0.csv", "CU_1.csv", etc.
+#' @inheritParams get_otg_col_specs
 #'
 #' @import purrr dplyr
 #' @importFrom readr read_csv
 #' @importFrom magrittr %>%
+#' @importFrom compare compare
 #' @export
 #' @return a data frame containing data of \code{otg_type}
 
@@ -23,47 +22,59 @@ read_otg_csv = function(path = ".",
                                      "Jam_3.csv",
                                      "Undercut_4.csv",
                                      "Discharge_5.csv",
-                                     "Discharge_Measurements_6.csv")) {
+                                     "DischargeMeasurements_6.csv")) {
 
   # files list of the otg_type
   file_list = get_file_nms(path) %>%
     dplyr::filter(file_nm == otg_type) %>%
-    dplyr::mutate(file_cnt = row_number()) %>%
+    #dplyr::mutate(file_cnt = row_number()) %>%
     split(list(.$path_nm))
 
-  # name of the first file of otg_type
-  first_otg_file = paste0(path, file_list[[1]]$path_nm)
+  # grab intended column specifications using get_otg_col_specs()
+  otg_col_specs = get_otg_col_specs(otg_type)
 
-  # extract column specifications from the first file
-  first_spec = readr::read_csv(first_otg_file) %>%
-    spec()
-
+  # map_df over file_list
   tmp_df = file_list %>%
     purrr::map_df(.id = "path_nm",
                   .f = function(x) {
 
-                    cat(paste("Reading", otg_type, "file from", x$folder_nm, "survey.", "\n"))
+                    cat(paste("Attempting to read", otg_type, "file from", x$folder_nm, "survey folder.", "\n"))
 
-                    # read in each .csv file of otg_type
+                    # get column specifications from file
+                    tmp_specs = readr::read_csv(paste0(path, x$path_nm), col_types = readr::cols()) %>%
+                      readr::spec()
+
+                    # CHECK 1: check if tmp_specs match the expected from get_otg_col_specs()
+                    # chk = identical(otg_col_specs, tmp_specs)
+                    # a "looser" version of identical() to allow for differing attributes
+                    chk = compare(otg_col_specs, tmp_specs, allowAll = T)[[1]]
+                    if(chk == FALSE) warning(paste("Column specifications in", otg_type, "file from", x$folder_nm, "survey folder do not match those defined in get_otg_col_specs().", "\n",
+                                                   all.equal(otg_col_specs, tmp_specs), "\n"))
+
+                    # CHECK 2: check if column names match
+                    chk = identical(names(otg_col_specs$cols), names(tmp_specs$cols))
+                    if(chk == FALSE) cat(paste("Column names in", otg_type, "file from", x$folder_nm, "survey folder do not match the expected names defined in get_otg_col_specs(). Check file format. Continuing...", "\n"))
+
+                    # CHECK 3: check if number of columns match
+                    chk = identical(length(otg_col_specs$cols), length(tmp_specs$cols))
+                    if(chk == FALSE) stop(paste("Fatal Error: Number of columns in", otg_type, "file from", x$folder_nm, "survey folder is unexpected."))
+
+                    # read in the single .csv file of otg_type
                     tmp = try(suppressWarnings(readr::read_csv(paste0(path, x$path_nm),
-                                                               col_types = cols())))
+                                                               col_types = otg_col_specs)))
 
-                    # ERROR MESSAGES
-
-                    # if nrow of tmp (a single file) is 0 or class(tmp) is error
-                    if(nrow(tmp) == 0 | class(tmp)[1] == "try-error") {
-                      cat(paste("Problem reading in", otg_type, "file from", x$folder_nm, "survey.", "\n"))
+                    # CHECK 4
+                    if(chk == FALSE | nrow(tmp) == 0 | class(tmp)[1] == "try-error") {
+                      cat(paste("Problem reading in", otg_type, "file from", x$folder_nm, "survey. Returning NULL", "\n"))
                       return(NULL)
                     }
+                    if(chk == TRUE) {
+                      cat(paste("Success!", "\n"))
+                      return(tmp)
+                    }
 
-                    # check that column specifications of tmp match those from the first file
-                    check = identical(first_spec, spec(tmp))
-                    if(check == FALSE) stop(paste("Bummer. Column specifications for", otg_type, "file from", x$folder_nm, "survey.", "\n",
-                                                  "do not match those in", first_otg_file, ". \n",
-                                                  "Please check your file formats and try again."))
-
-                    return(tmp)
                   })
 
   return(tmp_df)
+
 } # end read_otg_csv()
