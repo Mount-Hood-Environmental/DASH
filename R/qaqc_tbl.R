2109d220b98acb304538defff22b4d67e47f188f
Mike Ackerman <dirtpathwanderer@gmail.com>
2020-08-19 12:46:56 -0600

    added qaqc_tbl() function

---
+++ R/qaqc_tbl.R
@@ -0,0 +1,24 @@
+#' @title Initiate Blank QA/QC Results Tibble
+#'
+#' @description Create a blank tibble to store messages from QA/QC of on-the-ground
+#' collected data
+#'
+#' @author Mike Ackerman
+#'
+#' @importFrom tibble tibble
+#' @export
+#' @return a blank tibble
+
+qaqc_tbl = function() {
+
+  # create a blank tibble
+  tmp = tibble::tibble(
+    path_name = character(),
+    row_num = integer(),
+    col_name = integer(),
+    error_message = character()
+  )
+
+  return(tmp)
+
+} # end qaqc_tbl()
