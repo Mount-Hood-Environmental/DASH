d17dd5d83922ef029d42f2c3d9ea796f3d72ea78
Kevin See <Kevin.See@merck.com>
2020-08-28 15:48:28 -0700

    added code to run QC on all the various components of OTG data, and put the results together into a single tibble.

--- inst/Rscripts/02_qc_2019_otg.R
+++ inst/Rscripts/02_qc_2019_otg.R
@@ -12,6 +12,7 @@
 #-----------------------------
 # load necessary libraries
 #-----------------------------
+library(dplyr)
 
 #-------------------------
 # set NAS prefix, depending on operating system
@@ -28,6 +29,10 @@ if(.Platform$OS.type == 'unix') {
 #-----------------------------
 load(paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/lemhi/prepped/raw_DASH_2019_otg.rda"))
 
+# delete some rows with all missing values
+otg_data$jam <- dplyr::filter(otg_data$jam,
+                              !is.na(ObjectID))
+
 #-----------------------------
 # QA/QC surveyPoint_0.csv
 #-----------------------------
@@ -39,4 +44,66 @@ qc_surv_results = qc_survey(qc_df = otg_data$survey,
 #-----------------------------
 # QA/QC CU_1.csv
 #-----------------------------
-qc_cu_results = qc_cu()
+qc_cu_results = qc_cu(qc_df = otg_data$cu)
+
+#-----------------------------
+# QA/QC Wood_2.csv
+#-----------------------------
+qc_wood_results = qc_wood(qc_df = otg_data$wood)
+
+#-----------------------------
+# QA/QC Jam_3.csv
+#-----------------------------
+qc_jam_results = qc_jam(qc_df = otg_data$jam)
+
+#-----------------------------
+# QA/QC Undercut_4.csv
+#-----------------------------
+qc_undercut_results = qc_undercut(qc_df = otg_data$undercut)
+
+#-----------------------------
+# QA/QC Discharge_5.csv
+#-----------------------------
+qc_disch_results = qc_disch(qc_df = otg_data$discharge)
+
+#-----------------------------
+# QA/QC DischargeMeasurements_6.csv
+#-----------------------------
+qc_disch_meas_results = qc_disch_meas(qc_df = otg_data$discharge_measurements)
+
+
+#-----------------------------
+# Combine all QA/QC results
+#-----------------------------
+qc_all = qc_surv_results %>%
+  tibble::add_column(Source = "Survey",
+                     .before = 0) %>%
+  bind_rows(qc_cu_results %>%
+              tibble::add_column(Source = "CU",
+                                 .before = 0)) %>%
+  bind_rows(qc_wood_results %>%
+              tibble::add_column(Source = "Wood",
+                                 .before = 0)) %>%
+  bind_rows(qc_jam_results %>%
+              tibble::add_column(Source = "Jam",
+                                 .before = 0)) %>%
+  bind_rows(qc_undercut_results %>%
+              tibble::add_column(Source = "Undercut",
+                                 .before = 0)) %>%
+  bind_rows(qc_disch_results %>%
+              tibble::add_column(Source = "Discharge",
+                                 .before = 0)) %>%
+  bind_rows(qc_disch_meas_results %>%
+              tibble::add_column(Source = "DischargeMeasurements",
+                                 .before = 0))
+
+
+# where do all the QA/QC errors come from?
+janitor::tabyl(qc_all, Source)
+
+# one way to examine the QA/QC results
+qc_all %>%
+  filter(Source == "Jam") %>%
+  left_join(otg_data$jam) %>%
+  select(-(ParentGlobalID:Editor)) %>%
+  as.data.frame()
