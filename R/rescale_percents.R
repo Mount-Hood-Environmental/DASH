d26f848197ff051a4f6d64f8818985c3c8f578cc
Mike Ackerman <dirtpathwanderer@gmail.com>
2020-09-03 16:40:50 -0600

    added min_perc and max_perc arguments to rescale_percents()

--- R/rescale_percents.R
+++ R/rescale_percents.R
@@ -6,13 +6,17 @@
 #'
 #' @param data_df Input data.frame
 #' @param col_names character vector of columns that should sum to 100
+#' @param min_perc minimum sum of columns that should be re-scaled to 100
+#' @param max_perc maximum sum of columns that should be re-scaled to 100
 #'
 #' @import dplyr
 #' @export
 #' @return a tibble with corrected percentages that sum to 100
 
 rescale_percents <- function(data_df = NULL,
-                             col_names = NULL) {
+                             col_names = NULL,
+                             min_perc = 80,
+                             max_perc = 120) {
   stopifnot(!is.null(data_df),
             !is.null(col_names))
 
@@ -23,10 +27,11 @@ rescale_percents <- function(data_df = NULL,
     group_by(GlobalID) %>%
     mutate(sum_perc = sum(value, na.rm = T)) %>%
     ungroup() %>%
+    # if all values sum to 0 & every value is NA, change those values to 0
     mutate(value = if_else(sum_perc > 0 & is.na(value),
                            0,
                            value)) %>%
-    mutate(value = if_else(!sum_perc %in% c(0, 100),
+    mutate(value = if_else(!sum_perc %in% c(0, 100) & between(sum_perc, min_perc, max_perc),
                            value / sum_perc * 100,
                            value)) %>%
     mutate_at(vars(value),
