9d0ca85ed168c64d3e729e25d05e78967600020f
Mike Ackerman <dirtpathwanderer@gmail.com>
2020-10-02 11:55:17 -0600

    cleaned up 02 script

--- inst/Rscripts/02_qc_2019_otg.R
+++ inst/Rscripts/02_qc_2019_otg.R
@@ -2,10 +2,11 @@
 #
 # Purpose: A script to QA/QC 2019 on-the-ground (OTG) DASH data from
 # Survey 123. This is a follow-up to the script to read in the OTG data.
-# My intent is to later convert this script into a series of functions.
+# We QC the raw data, resolve some issues identified, flag the remaining
+# and output a .csv of those, and re-write the data.
 #
 # Created: August 12, 2020
-#   Last Modified: September 3, 2020
+#   Last Modified: October 2, 2020
 #
 # Notes:
 rm(list = ls())
@@ -13,11 +14,12 @@ rm(list = ls())
 #-----------------------------
 # load necessary libraries
 #-----------------------------
-library(dplyr)
-library(readr)
 library(tidyverse)
-library(janitor)
-library(beepr)
+
+#library(readr)
+#library(tidyverse)
+#library(janitor)
+#library(beepr)
 
 #-------------------------
 # set NAS prefix, depending on operating system
@@ -35,87 +37,80 @@ if(.Platform$OS.type == 'unix') {
 load(paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/lemhi/prepped/raw_DASH_2019_otg.rda"))
 
 #-----------------------------
-# QC each otg_type, separately
+# As an example, QC just a couple otg_types, separately
 #-----------------------------
-# qc_surv_results = qc_survey(qc_df = otg_data$survey)         # surveyPoint_0.csv
-# qc_cu_results = qc_cu(qc_df = otg_data$cu)                   # CU_1.csv
-# qc_wood_results = qc_wood(qc_df = otg_data$wood)             # Wood_2.csv
-# qc_jam_results = qc_jam(qc_df = otg_data$jam)                # Jam_3.csv
-# qc_undercut_results = qc_undercut(qc_df = otg_data$undercut) # Undercut_4.csv
-# qc_disch_results = qc_disch(qc_df = otg_data$discharge)      # Discharge_5.csv
-# qc_disch_meas_results = qc_disch_meas(qc_df = otg_data$discharge_measurements)
-#   # DischargeMeasurements_6.csv
+qc_cu_results = qc_cu(qc_df = otg_raw$cu)
+qc_wood_results = qc_wood(qc_df = otg_raw$wood)
+
+# combine those together
+qc_some = qc_cu_results %>%
+  tibble::add_column(source = "CU",
+                     .before = 0) %>%
+  bind_rows(qc_wood_results %>%
+              tibble::add_column(source = "Wood",
+                                 .before = 0))
+
+# or using the qc_wrapper function instead
+qc_some2 = qc_wrapper(survey_df = NULL,
+                      cu_df = otg_raw$cu,
+                      wood_df = otg_raw$wood,
+                      jam_df = NULL,
+                      undercut_df = NULL,
+                      discharge_df = NULL,
+                      disch_meas_df = NULL)
 
 #-----------------------------
-# Combine all separate QC results
+# For real, let's QC all the raw data
 #-----------------------------
-# qc_all = qc_surv_results %>%
-#   tibble::add_column(source = "Survey",
-#                      .before = 0) %>%
-#   bind_rows(qc_cu_results %>%
-#               tibble::add_column(source = "CU",
-#                                  .before = 0)) %>%
-#   bind_rows(qc_wood_results %>%
-#               tibble::add_column(source = "Wood",
-#                                  .before = 0)) %>%
-#   bind_rows(qc_jam_results %>%
-#               tibble::add_column(source = "Jam",
-#                                  .before = 0)) %>%
-#   bind_rows(qc_undercut_results %>%
-#               tibble::add_column(source = "Undercut",
-#                                  .before = 0)) %>%
-#   bind_rows(qc_disch_results %>%
-#               tibble::add_column(source = "Discharge",
-#                                  .before = 0)) %>%
-#   bind_rows(qc_disch_meas_results %>%
-#               tibble::add_column(source = "DischargeMeasurements",
-#                                  .before = 0))
+qc_raw = qc_wrapper(survey_df = otg_raw$survey,
+                    cu_df = otg_raw$cu,
+                    wood_df = otg_raw$wood,
+                    jam_df = otg_raw$jam,
+                    undercut_df = otg_raw$undercut,
+                    discharge_df = otg_raw$discharge,
+                    disch_meas_df = otg_raw$discharge_measurements)
 
 #-----------------------------
-# Using qc_wrapper() instead
-#-----------------------------
-# all QC options
-qc_all = qc_wrapper(survey_df = otg_data$survey,
-                    cu_df = otg_data$cu,
-                    wood_df = otg_data$wood,
-                    jam_df = otg_data$jam,
-                    undercut_df = otg_data$undercut,
-                    discharge_df = otg_data$discharge,
-                    disch_meas_df = otg_data$discharge_measurements)
-
-# just a couple, for example
-# qc_some = qc_wrapper(survey_df = NULL,
-#                     cu_df = NULL,
-#                     wood_df = otg_data$wood,
-#                     jam_df = otg_data$jam,
-#                     undercut_df = NULL,
-#                     discharge_df = NULL,
-#                     disch_meas_df = NULL)
-
-#-----------------------------
-# Examine QC results
+# Quick look at the initial QC results
 #-----------------------------
 # where do all the QA/QC errors come from?
-janitor::tabyl(qc_all, source)
+janitor::tabyl(qc_raw, source)
 
-# The vast majority of QC errors are in the CU data, and of those, most are related to ocular substrate estimates
-# and fish cover estimates. Let's examine those further...
+# a large chunk are from the "CU" data, examine a little further...
+qc_raw %>%
+  filter(source == "CU") %>%
+  count(error_message) %>%
+  print(n = 100)
+
+# Note many errors in the "CU" data are related to ocular substrate estimates not summing to 100
+# and fish cover estimates not summing to within an expected range. Let's resolve those using
+# some functions we've created within the DASH package
 
 # Ocular substrate estimates
-qc_all %>%
+qc_raw %>%
   filter(source == "CU") %>%
   filter(grepl("Ocular estimates sum", error_message)) %>%
-  left_join(otg_data$cu) %>%
+  left_join(otg_raw$cu) %>%
   select(source:GlobalID,
          `Channel Unit Type`,
          `Sand/Fines 2mm`:`Boulder 256mm`) %>%
   mutate(oc_cover = rowSums(.[5:8], na.rm = T))
 
-# Cover estimates
-qc_all %>%
+# couple ocular substrate estimates that sum to 90, resolve using rescale_values()
+otg_raw$cu = rescale_values(data_df = otg_raw$cu,
+                            col_names = c("Sand/Fines 2mm",
+                                          "Gravel 2-64mm",
+                                          "Cobble 64-256mm",
+                                          "Boulder 256mm"),
+                            min_value = 90,
+                            max_value = 110,
+                            sum_to = 100)
+
+# Fish cover estimates
+qc_raw %>%
   filter(source == "CU") %>%
   filter(grepl("Cover values sum", error_message)) %>%
-  left_join(otg_data$cu) %>%
+  left_join(otg_raw$cu) %>%
   select(source:GlobalID,
          `Overhanging Cover`:`Total No Cover`) %>%
   mutate(sum_cover = rowSums(.[4:8], na.rm = T)) %>%
@@ -123,104 +118,74 @@ qc_all %>%
   #select(sum_cover) %>%
   table()
 
-# We've created the rescale_values() and fix_fish_cover() argument to resolve many of those...
+# lots of issues with fish cover estimates, resolve some using fix_fish_cover()...
+otg_raw$cu = fix_fish_cover(cu_df = otg_raw$cu,
+                            cover_cols = c("Overhanging Cover",
+                                           "Aquatic Vegetation",
+                                           "Woody Debris Cover",
+                                           "Artificial Cover",
+                                           "Total No Cover"))
 
-# First, ocular estimates, they should be a bit easier as they should sum to 100.
-# ocular substrate columns
-oc_cols = c("Sand/Fines 2mm",
-            "Gravel 2-64mm",
-            "Cobble 64-256mm",
-            "Boulder 256mm")
+#-----------------------------
+# Re-run the QC after resolving some ocular and cover issues
+#-----------------------------
+qc_raw = qc_wrapper(survey_df = otg_raw$survey,
+                    cu_df = otg_raw$cu,
+                    wood_df = otg_raw$wood,
+                    jam_df = otg_raw$jam,
+                    undercut_df = otg_raw$undercut,
+                    discharge_df = otg_raw$discharge,
+                    disch_meas_df = otg_raw$discharge_measurements)
 
-# re-scale ocular estimate columns
-otg_data$cu = rescale_values(data_df = otg_data$cu,
-                             col_names = oc_cols,
-                             min_value = 80,
-                             max_value = 120,
-                             sum_to = 100)
-
-# Now, fish cover estimates...
-# fish cover columns
-cover_cols = c("Overhanging Cover",
-               "Aquatic Vegetation",
-               "Woody Debris Cover",
-               "Artificial Cover",
-               "Total No Cover")
-
-# resolve fish cover issues using fix_fish_cover()
-otg_data$cu = fix_fish_cover(cu_df = otg_data$cu,
-                             cover_cols = cover_cols)
-
-# Now, re-run the QC and write out those results
-qc_all = qc_wrapper(survey_df = otg_data$survey,
-                    cu_df = otg_data$cu,
-                    wood_df = otg_data$wood,
-                    jam_df = otg_data$jam,
-                    undercut_df = otg_data$undercut,
-                    discharge_df = otg_data$discharge,
-                    disch_meas_df = otg_data$discharge_measurements)
-
-# where do all the QA/QC errors come from?
-janitor::tabyl(qc_all, source)
-
-# Write out QC results
-output_path = paste0(nas_prefix,
-                     "/data/habitat/DASH/OTG/2019/lemhi/1_formatted_csvs/qc_results_DASH_2019_",
-                     format(Sys.Date(), format = "%Y%m%d"),
-                     ".csv")
-readr::write_csv(qc_all, output_path)
-
-# Now, let's examine some of the remaining QC flags
-#####
-# SURVEY
+#-----------------------------
+# Examine the remaining QC errors in R
+#-----------------------------
 # latitude, longitude errors
-qc_all %>%
+qc_raw %>%
   filter(source == "Survey") %>%
-  left_join(otg_data$survey) %>%
+  left_join(otg_raw$survey) %>%
   select(source:error_message, x:y)
 
-#####
-# CU
 # `Maximum Depth (m)` errors
-qc_all %>%
+qc_raw %>%
   filter(source == "CU") %>%
   filter(grepl("Column Maximum Depth", error_message)) %>%
-  left_join(otg_data$cu) %>%
+  left_join(otg_raw$cu) %>%
   select(source:error_message, `Channel Unit Type`, `Maximum Depth (m)`) %>%
   #View()
   janitor::tabyl(`Channel Unit Type`)
 
 # Duplicate channel units
-qc_all %>%
+qc_raw %>%
   filter(source == "CU") %>%
   filter(grepl("appears more than once", error_message)) #%>%
   #View()
 
 # Thalweg exit depth
-qc_all %>%
+qc_raw %>%
   filter(source == "CU") %>%
   filter(grepl("Thalweg exit depth", error_message)) %>%
-  left_join(otg_data$cu) %>%
+  left_join(otg_raw$cu) %>%
   select(source:error_message,
          `Channel Unit Number`,
          `Channel Segment Number`,
          `Thalweg Exit Depth (m)`)
 
 # Ocular substrate estimates
-qc_all %>%
+qc_raw %>%
   filter(source == "CU") %>%
   filter(grepl("Ocular estimates sum", error_message)) %>%
-  left_join(otg_data$cu) %>%
+  left_join(otg_raw$cu) %>%
   select(source:GlobalID,
          `Channel Unit Type`,
          `Sand/Fines 2mm`:`Boulder 256mm`) %>%
   mutate(oc_cover = rowSums(.[5:8], na.rm = T))
 
 # Cover estimates
-qc_all %>%
+qc_raw %>%
   filter(source == "CU") %>%
   filter(grepl("Cover values sum", error_message)) %>%
-  left_join(otg_data$cu) %>%
+  left_join(otg_raw$cu) %>%
   select(source:GlobalID,
          `Overhanging Cover`:`Total No Cover`) %>%
   mutate(sum_cover = rowSums(.[4:8], na.rm = T)) %>%
@@ -229,33 +194,32 @@ qc_all %>%
   table()
 
 # Pebble counts
-qc_all %>%
+qc_raw %>%
   filter(source == "CU") %>%
   filter(grepl("Pebble size values", error_message)) %>%
-  left_join(otg_data$cu) %>%
+  left_join(otg_raw$cu) %>%
   select(path_nm:GlobalID,
          contains("Pebble")) #%>%
   #View()
 
-#####
-# WOOD
-qc_all %>%
+# Wood
+qc_raw %>%
   filter(source == "Wood") %>%
   filter(grepl("Column Large Wood Number is <blank> or NA.", error_message)) %>%
-  left_join(otg_data$wood)
+  left_join(otg_raw$wood)
 
-qc_all %>%
+qc_raw %>%
   filter(source == "Wood") %>%
   filter(grepl("Column Diameter", error_message)) %>%
-  left_join(otg_data$wood) %>%
+  left_join(otg_raw$wood) %>%
   select(path_nm:GlobalID,
          ObjectID:`Ballasted?`) #%>%
   #View()
 
-qc_all %>%
+qc_raw %>%
   filter(source == "Wood") %>%
   filter(grepl("Wet|Channel Forming|Ballasted", error_message)) %>%
-  left_join(otg_data$wood) %>%
+  left_join(otg_raw$wood) %>%
   select(path_nm:GlobalID,
          `Wet?`,
          `Channel Forming?`,
@@ -264,119 +228,50 @@ qc_all %>%
   filter(is.na(value)) #%>%
   #View()
 
-qc_all %>%
+qc_raw %>%
   filter(source == "Wood") %>%
   filter(grepl("Length is less than or equal to the diameter|falls outside of the expected", error_message)) %>%
-  left_join(otg_data$wood) %>%
+  left_join(otg_raw$wood) %>%
   select(path_nm:GlobalID,
          `Length (m)`,
          `Diameter (m)`)
 
-#####
-# JAM
-qc_all %>%
+# Jam
+qc_raw %>%
   filter(source == "Jam") %>%
-  left_join(otg_data$jam) %>%
+  left_join(otg_raw$jam) %>%
   select(-(ParentGlobalID:Editor)) %>%
   as.data.frame() #%>%
   #View()
 
-#####
-# UNDERCUT
-qc_all %>%
+# Undercut
+qc_raw %>%
   filter(source == "Undercut") %>%
-  left_join(otg_data$undercut) %>%
+  left_join(otg_raw$undercut) %>%
   select(path_nm:error_message,
          `Undercut Number`:`Width 75% (m)`) %>%
   as.data.frame() #%>%
   #View()
 
-#####
-# DISCHARGE MEASUREMENTS
-qc_all %>%
+# Discharge measurements
+qc_raw %>%
   filter(source == "DischargeMeasurements") %>%
-  left_join(otg_data$discharge_measurements) %>%
+  left_join(otg_raw$discharge_measurements) %>%
   select(path_nm:error_message,
          ObjectID:`Station Velocity`) %>%
   as.data.frame() #%>%
   #View()
 
-######################################################################
-# At this point, I fixed many errors identified by the QC 2020-09-02 #
-######################################################################
-# re-import QC'd data
-path = paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/lemhi/2_qcd_csvs/")
-otg_qcd_data = read_otg_csv_wrapper(path = path,
-                                    otg_type_list = c("surveyPoint_0.csv",
-                                                      "CU_1.csv",
-                                                      "Wood_2.csv",
-                                                      "Jam_3.csv",
-                                                      "Undercut_4.csv",
-                                                      "Discharge_5.csv",
-                                                      "DischargeMeasurements_6.csv"),
-                                    otg_type_names = c("survey",
-                                                       "cu",
-                                                       "wood",
-                                                       "jam",
-                                                       "undercut",
-                                                       "discharge",
-                                                       "discharge_measurements"))
-beepr::beep(sound = "ping")
-
-# Make our fixes to ocular substrate and fish cover estimates, which are a big chunk of the issues
-otg_qcd_data$cu = rescale_values(data_df = otg_qcd_data$cu,
-                                 col_names = c("Sand/Fines 2mm",
-                                               "Gravel 2-64mm",
-                                               "Cobble 64-256mm",
-                                               "Boulder 256mm"),
-                                 min_value = 80,
-                                 max_value = 120,
-                                 sum_to = 100)
-
-otg_qcd_data$cu = fix_fish_cover(cu_df = otg_qcd_data$cu,
-                                 cover_cols = c("Overhanging Cover",
-                                                "Aquatic Vegetation",
-                                                "Woody Debris Cover",
-                                                "Artificial Cover",
-                                                "Total No Cover"))
-
-
-# perform QC on the qc'd data
-qc_all = qc_wrapper(survey_df = otg_qcd_data$survey,
-                    cu_df = otg_qcd_data$cu,
-                    wood_df = otg_qcd_data$wood,
-                    jam_df = otg_qcd_data$jam,
-                    undercut_df = otg_qcd_data$undercut,
-                    discharge_df = otg_qcd_data$discharge,
-                    disch_meas_df = otg_qcd_data$discharge_measurements)
-
-# Write out remaining QC results
+# write out remaining QC error messages
 output_path = paste0(nas_prefix,
-                     "/data/habitat/DASH/OTG/2019/lemhi/2_qcd_csvs/qc_results_DASH_2019_",
+                     "/data/habitat/DASH/OTG/2019/lemhi/1_formatted_csvs/qc_results_DASH_2019_",
                      format(Sys.Date(), format = "%Y%m%d"),
                      ".csv")
-readr::write_csv(qc_all, output_path)
+readr::write_csv(qc_raw, output_path)
 
-# save the qc'd data
-save(otg_qcd_data,
-     file = paste0(nas_prefix,"/data/habitat/DASH/OTG/2019/lemhi/prepped/qcd_DASH_2019_otg.rda"))
+# At this point, someone very familiar with the OTG data (field technician, field coordinator)
+# should likely intervene, review the remaining QC errors that we just wrote out, attempt to resolve those,
+# and ideally make notes for those that can't be resolved, and for those than can, identify how
+# the issue was resolved.
 
-#-----------------------------
-# compare the raw formatted data to the data where QC errors have been/will be addressed
-#-----------------------------
-
-# the directories to compare
-path1 = paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/lemhi/1_formatted_csvs/")
-path2 = paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/lemhi/old_2_qcd_csvs/")
-
-# use compare_folders()
-qc_changes = compare_folders(path1 = path1,
-                             path2 = path2)
-save(qc_changes,
-     file = paste0(nas_prefix, "data/habitat/DASH/OTG/2019/lemhi/prepped/qc_resolutions.rda"))
-
-### END SCRIPT
-
-
-
-
+# END SCRIPT
