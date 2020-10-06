e2994e7d89c521797469c539f7195884467a84b8
Mike Ackerman <dirtpathwanderer@gmail.com>
2020-10-06 07:37:44 -0600

    started script 4

--- inst/Rscripts/04_rollup_2019_otg.R
+++ inst/Rscripts/04_rollup_2019_otg.R
@@ -1,17 +1,17 @@
 # Authors: Mike Ackerman
 #
 # Purpose: A script to clean and prep QC'd 2019 on-the-ground (OTG) DASH data.
-# This is a follow-up to the scripts to read in and QC the OTG data.
-# My intent is to later convert this script into a series of functions.
-#
-# Created: September 3, 2020
-#   Last Modified:
+# This script follows others to import and QC the raw data.
+# Created: October 6, 2020
+#   Lat Modified: October 6, 2020
 #
 # Notes:
+rm(list = ls())
 
 #-----------------------------
 # load necessary libraries
 #-----------------------------
+library(tidyverse)
 
 #-------------------------
 # set NAS prefix, depending on operating system
@@ -24,6 +24,18 @@ if(.Platform$OS.type == 'unix') {
 }
 
 #-----------------------------
-# load the otg_qcd_data list of dfs
+# load the otg_qcd list of dfs for lemhi and nf salmon
 #-----------------------------
-load(paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/lemhi/prepped/qcd_DASH_2019_otg.rda"))
+lemhi_otg = load(paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/lemhi/prepped/qcd_DASH_2019_otg.rda")) %>%
+  get()
+nfsal_otg = load(paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/nf_salmon/prepped/qcd_DASH_2019_otg.rda")) %>%
+  get()
+
+# and merge them
+otg_qcd = purrr::map2(lemhi_otg,
+                      nfsal_otg,
+                      dplyr::full_join)
+
+# clean up
+rm(lemhi_otg, nfsal_otg)
+
