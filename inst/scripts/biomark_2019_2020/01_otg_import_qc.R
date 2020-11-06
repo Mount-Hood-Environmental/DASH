a5983cdfca57996b37eb6107efce12569f962994
Mike Ackerman <dirtpathwanderer@gmail.com>
2020-11-06 13:13:30 -0700

    finished DRAFT pipeline for 2019 & 2020 DASH OTG data

--- inst/scripts/biomark_2019_2020/01_otg_import_qc.R
+++ inst/scripts/biomark_2019_2020/01_otg_import_qc.R
@@ -15,7 +15,8 @@ rm(list = ls())
 # load necessary libraries
 #-----------------------------
 library(beepr)
-#library(tidyverse)
+library(janitor)
+library(tidyverse)
 #library(DASH)
 devtools::load_all()
 
@@ -284,14 +285,135 @@ for (yw in yr_wtsd) {
 
 } # end qc final data loop
 
-#######################################################################################
 #-----------------------------
-# Save the QC'd data, but before we do, let's clean_names()
+# Load, join, clean, and rollup data and the channel unit scale
 #-----------------------------
-# clean names
-otg_qcd = lapply(otg_qcd, clean_names)
+otg_path = paste0(nas_prefix,
+                  "/data/habitat/DASH/OTG/")
 
-save(otg_qcd,
-     file = paste0(nas_prefix,"/data/habitat/DASH/OTG/2019/", trib, "/prepped/qcd_DASH_2019_otg.rda"))
+# list of otg_qcd files in otg_path
+otg_qcd_files = list.files(path = otg_path,
+                           pattern = "^otg_qcd\\.rda$",
+                           recursive = T) %>%
+  paste0(otg_path, .)
+
+# load the otg_qcd files
+lem_otg_qcd = load(otg_qcd_files[1]) %>%
+  get() %>%
+  lapply(clean_names)
+nfs_otg_qcd = load(otg_qcd_files[2]) %>%
+  get() %>%
+  lapply(clean_names)
+sec_otg_qcd = load(otg_qcd_files[3]) %>%
+  get() %>%
+  lapply(clean_names)
+
+# join using map2
+otg = purrr::map2(lem_otg_qcd,
+                  nfs_otg_qcd,
+                  dplyr::full_join) %>%
+  purrr::map2(sec_otg_qcd,
+              dplyr::full_join)
+
+# clean up
+rm(lem_otg_qcd,
+   nfs_otg_qcd,
+   sec_otg_qcd)
+
+#-----------------------------
+# clean cu data and join site info to it
+#-----------------------------
+# clean survey names
+otg$survey = otg$survey %>%
+  mutate(site_name = gsub("_.*", "", site_name)) %>%
+  mutate(site_name = gsub(" ", "", site_name))
+
+# CU
+cu_cu = rollup_cu(cu_df = otg$cu,
+                  survey_df = otg$survey)
+
+#-----------------------------
+# start rolling up data to cu scale
+#-----------------------------
+cu_wood = rollup_cu_wood(wood_df = otg$wood)
+cu_jam = rollup_cu_jam(jam_df = otg$jam)
+cu_undercut = rollup_cu_undercut(undercut_df = otg$undercut)
+
+# discharge is a little tricky
+cu_discharge = rollup_cu_discharge(discharge_df = otg$discharge,
+                                   discharge_meas_df = otg$discharge_measurements) %>%
+  # Add the below to the rollup_cu_discharge() function?
+  left_join(otg$survey %>%
+              select(global_id,
+                     site_name),
+            by = c("parent_global_id" = "global_id")) %>%
+  mutate(cu_id = paste(site_name,
+                       "01",
+                       str_pad(discharge_location_bos_tos_cu_number, 3, pad = "0"),
+                       sep = "_")) %>%
+  select(site_name,
+         cu_id,
+         discharge_cms,
+         discharge_location_bos_tos_cu_number,
+         everything()) %>%
+  left_join(cu_cu %>%
+              select(parent_global_id,
+                     cu_id,
+                     channel_unit_number) %>%
+              group_by(parent_global_id) %>%
+              summarise(min_cu = min(channel_unit_number),
+                        max_cu = max(channel_unit_number))) %>%
+  select(cu_id,
+         discharge_cms)
+
+# there's an error here somewhere in the Secesh discharge data, rollup, something
+# Also need to resolve the BOS and TOS issue. How do I assign a CU number to the BOS and TOS discharge measurements?
+
+#-----------------------------
+# join everything together at cu scale
+#-----------------------------
+dash_otg_cu = list(cu_cu %>%
+                     rename(site_id = global_id),
+                   cu_wood,
+                   cu_jam,
+                   cu_undercut) %>%
+  purrr::reduce(left_join,
+                by = "parent_global_id") %>%
+  left_join(cu_discharge)
+
+#######################################################################################
+# write joined data
+# output_path = paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/prepped/")
+# save(otg,
+#      file = paste0(output_path, "DASH_2019_otg.rda"))
+#
+# # and csvs (for now)
+# write_csv(otg$survey, paste0(output_path, "survey.csv"))
+# write_csv(otg$cu, paste0(output_path, "cu.csv"))
+# write_csv(otg$wood, paste0(output_path, "wood.csv"))
+# write_csv(otg$jam, paste0(output_path, "jam.csv"))
+# write_csv(otg$undercut, paste0(output_path, "undercut.csv"))
+# write_csv(otg$discharge, paste0(output_path, "discharge.csv"))
+# write_csv(otg$discharge_measurements, paste0(output_path, "discharge_measurements.csv"))
+#-----------------------------
+# join everything together at cu scale
+#-----------------------------
+dash_otg_cu = list(cu_cu %>%
+                     rename(site_id = global_id),
+                   cu_wood,
+                   cu_jam,
+                   cu_undercut) %>%
+  purrr::reduce(left_join,
+                by = "parent_global_id") %>%
+  left_join(cu_discharge)
+
+#-----------------------------
+# write results
+#-----------------------------
+save(dash_otg_cu,
+     file = paste0(nas_prefix, "data/habitat/DASH/OTG/prepped/dash_2019_2020_otg_cu.rda"))
+
+write_csv(dash_otg_cu,
+          paste0(nas_prefix, "data/habitat/DASH/OTG/prepped/dash_2019_2020_otg_cu.csv"))
 
 ### END SCRIPT
