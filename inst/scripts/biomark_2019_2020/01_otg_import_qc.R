98230af201deaae8c23659fc94a4e296c688cbb0
Mike Ackerman <dirtpathwanderer@gmail.com>
2020-11-10 14:37:07 -0700

    clean import QC script

--- inst/scripts/biomark_2019_2020/01_otg_import_qc.R
+++ inst/scripts/biomark_2019_2020/01_otg_import_qc.R
@@ -384,32 +384,6 @@ dash_otg_cu = list(cu_cu %>%
                 by = "parent_global_id") %>%
   left_join(cu_discharge)
 
-#######################################################################################
-# write joined data
-# output_path = paste0(nas_prefix, "/data/habitat/DASH/OTG/2019/prepped/")
-# save(otg,
-#      file = paste0(output_path, "DASH_2019_otg.rda"))
-#
-# # and csvs (for now)
-# write_csv(otg$survey, paste0(output_path, "survey.csv"))
-# write_csv(otg$cu, paste0(output_path, "cu.csv"))
-# write_csv(otg$wood, paste0(output_path, "wood.csv"))
-# write_csv(otg$jam, paste0(output_path, "jam.csv"))
-# write_csv(otg$undercut, paste0(output_path, "undercut.csv"))
-# write_csv(otg$discharge, paste0(output_path, "discharge.csv"))
-# write_csv(otg$discharge_measurements, paste0(output_path, "discharge_measurements.csv"))
-#-----------------------------
-# join everything together at cu scale
-#-----------------------------
-dash_otg_cu = list(cu_cu %>%
-                     rename(site_id = global_id),
-                   cu_wood,
-                   cu_jam,
-                   cu_undercut) %>%
-  purrr::reduce(left_join,
-                by = "parent_global_id") %>%
-  left_join(cu_discharge)
-
 #-----------------------------
 # write results
 #-----------------------------
