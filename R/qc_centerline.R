42da9e6a349c8d1915c11f3edf5fd4363f395f46
Kevin See <Kevin.See@merck.com>
2020-12-08 14:51:02 -0800

    updated function to use new qc_tbl and check_na functions

--- R/qc_centerline.R
+++ R/qc_centerline.R
@@ -15,6 +15,7 @@
 #' @return a tibble with QC results
 
 qc_centerline <- function(cl_sf = NULL,
+                          data_id = "file_row_id",
                           cols_to_check_nas = c("CU_Number",
                                                 "Site_ID",
                                                 "StreamName",
@@ -27,15 +28,19 @@ qc_centerline <- function(cl_sf = NULL,
   # Starting message
   cat("Starting QC on  centerline data. \n")
 
+  # cl_sf %>%
+  #   rename(GlobalID = file_row_id)
+
   # Initiate qc_tmp
-  qc_tmp = qc_tbl()
+  qc_tmp = qc_tbl(data_id)
 
   #####
   # CHECK 1: Are there NAs in these columns?
   tmp = cl_sf %>%
     sf::st_drop_geometry() %>%
     dplyr::as_tibble() %>%
-    check_na(cols_to_check_nas)
+    check_na(data_id,
+             cols_to_check_nas)
     # dplyr::mutate(line_num = 1:n()) %>%
     # dplyr::select(line_num, any_of(cols_to_check_nas)) %>%
     # dplyr::filter_at(., vars(cols_to_check_nas), any_vars(is.na(.))) %>%
@@ -48,7 +53,7 @@ qc_centerline <- function(cl_sf = NULL,
     # dplyr::mutate(error_message = paste0("Column ", col_name, " is <blank> or NA on line ", line_num, ".")) %>%
     # dplyr::select(line_num, error_message)
 
-  if( nrow((tmp) > 0 ) qc_tmp = rbind(qc_tmp, tmp)
+  if( nrow(tmp) > 0 ) qc_tmp = rbind(qc_tmp, tmp)
 
   #####
   # CHECK 2: Are any channel units labeled 0?
@@ -57,12 +62,12 @@ qc_centerline <- function(cl_sf = NULL,
     dplyr::as_tibble() %>%
     # dplyr::select(line_num, CU_Number) %>%
     filter(CU_Number == 0) %>%
-    dplyr::mutate(error_message = paste0("Channel unit is labeled 0.")) %>%
-    dplyr::select(path_nm, GlobalID, error_message)
+    dplyr::mutate(error_message = "Channel unit is labeled 0.") %>%
+    dplyr::select(path_nm, any_of(data_id), error_message)
 
   if( nrow(cu_0) > 0 ) {
     cat( nrow(cu_0), "channel units labeled 0. Adding to QC results. \n")
-    qc_tmp = rbind(qc_tmp, tmp)
+    qc_tmp = rbind(qc_tmp, cu_0)
   }
 
   # return qc results
