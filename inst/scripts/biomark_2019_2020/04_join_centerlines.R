8127b3bad7caffdd919cdc42f9f1c13c42d3f5f6
Kevin See <Kevin.See@merck.com>
2020-12-08 16:05:11 -0800

    added a QC function, and code to save the results. Started code to join the OTG CU data with the centerline spatial data.

--- inst/scripts/biomark_2019_2020/04_join_centerlines.R
+++ inst/scripts/biomark_2019_2020/04_join_centerlines.R
@@ -17,6 +17,7 @@ rm(list = ls())
 #-----------------------------
 library(dplyr)
 library(purrr)
+library(stringr)
 library(janitor)
 library(sf)
 #library(DASH)
@@ -43,9 +44,13 @@ cl_list = list.files(path = cl_path,
   paste(cl_path, ., sep = "/") %>%
   as.list() %>%
   map(.f = function(x) {
-    read_sf(x)
+    read_sf(x) %>%
+      dplyr::mutate(path_nm = stringr::str_remove(x, cl_path)) %>%
+      select(path_nm,
+             everything())
   })
 
+cl_sf = NULL
 for(i in 1:length(cl_list)) {
   if(i == 1) {
     cl_sf = cl_list[[i]]
@@ -54,19 +59,37 @@ for(i in 1:length(cl_list)) {
                   cl_list[[i]])
   }
 }
+cl_sf = cl_sf %>%
+  mutate(file_row_id = 1:n()) %>%
+  select(path_nm,
+         file_row_id,
+         everything())
 
 rm(cl_list)
 
-cl_sf %>%
-  select(Site_ID) %>%
-  plot()
+# do a little QC
+cl_qc = qc_centerline(cl_sf)
 
-cl_sf %>%
-  filter(is.na(Site_ID) |
-           CU_Number == 0 |
-           is.na(CU_Type))
-cl_sf %>%
-  filter(CU_Number == 0)
+# save the file as a csv
+# add a local object ID to depict the row number within each file
+cl_qc %>%
+  inner_join(cl_sf %>%
+               group_by(path_nm) %>%
+               mutate(object_id = 1:n()) %>%
+               as_tibble() %>%
+               select(path_nm, file_row_id, object_id)) %>%
+  select(path_nm, file_row_id, object_id, everything()) %>%
+  arrange(path_nm, file_row_id, error_message) %>%
+  write_csv(paste0(cl_path,
+                 "/QC_centerlines_",
+                 format(Sys.Date(), format = "%Y%m%d"),
+                 ".csv"))
+
+
+cl_qc %>%
+  select(file_row_id) %>%
+  distinct() %>%
+  left_join(cl_sf)
 
 #-------------------------
 # load QC'd OTG data
@@ -99,25 +122,62 @@ for(i in 1:length(otg_list)) {
 # clean up
 rm(otg_list)
 
+# clean up some site names to match centerline file
+otg_all$survey = otg_all$survey %>%
+  rename(Site_ID = site_name) %>%
+  mutate(Site_ID = recode(Site_ID,
+                          "Lowerlemhi3_2019" = "LowerLemhi3_2019"))
+
 #-------------------------
 # join the OTG data
 #-------------------------
 cl_sf %>%
-  left_join(otg_all$survey)
+  rename(cl_path = path_nm) %>%
+  anti_join(otg_all$survey) %>%
+  tabyl(Site_ID)
 
-unique(cl_sf$Site_ID)[unique(cl_sf$Site_ID) %in% unique(otg_all$survey$site_name)]
-unique(cl_sf$Site_ID)[!unique(cl_sf$Site_ID) %in% unique(otg_all$survey$site_name)]
+# these site names are shared between the centerlines and the OTG data
+unique(cl_sf$Site_ID)[unique(cl_sf$Site_ID) %in% unique(otg_all$survey$Site_ID)]
+# these sites are in the centerline data but not the OTG
+unique(cl_sf$Site_ID)[!unique(cl_sf$Site_ID) %in% unique(otg_all$survey$Site_ID)]
+# these sites are in the OTG data but not the centerlines
+unique(otg_all$survey$Site_ID)[!unique(otg_all$survey$Site_ID) %in% unique(cl_sf$Site_ID)]
 
-unique(otg_all$survey$site_name)[!unique(otg_all$survey$site_name) %in% unique(cl_sf$Site_ID)]
+cl_sf %>%
+  inner_join(otg_all$cu %>%
+               select(-c(creation_date:editor),
+                      -path_nm,
+                      -object_id) %>%
+               rename(CU_Number = channel_unit_number,
+                      Seg_Number = channel_segment_number) %>%
+               left_join(otg_all$survey %>%
+                           select(parent_global_id = global_id,
+                                  Site_ID,
+                                  survey_date))) %>%
+  # filter(Site_ID == "Barton_2019",
+  #        CU_Number %in% 15:17)
+  as_tibble() %>%
+  tabyl(CU_Type, channel_unit_type)
 
-otg_all$cu %>%
-  left_join(otg_all$survey %>%
-              select(parent_global_id = global_id,
-                     site_name,
-                     survey_date)) %>%
-  filter(site_name == "Barton_2019",
-         channel_unit_number == 16) %>%
-  as.data.frame()
+cl_sf %>%
+  inner_join(otg_all$cu %>%
+               select(-c(creation_date:editor),
+                      -path_nm,
+                      -object_id) %>%
+               rename(CU_Number = channel_unit_number,
+                      Seg_Number = channel_segment_number) %>%
+               left_join(otg_all$survey %>%
+                           select(parent_global_id = global_id,
+                                  Site_ID,
+                                  survey_date))) %>%
+  # filter(Site_ID == "BigTimber1_2019") %>%
+  filter(grepl('Hayden', Site_ID)) %>%
+  ggplot() +
+  geom_sf(aes(color = CU_Number)) +
+  scale_color_viridis_c() +
+  theme_bw()
+
+
 
 otg_all$wood %>%
   left_join(otg_all$cu %>%
