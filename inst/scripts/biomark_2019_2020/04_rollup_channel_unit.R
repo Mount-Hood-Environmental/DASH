8f63b1dd841627ca499941fbd1211d51966f77c6
Kevin See <Kevin.See@merck.com>
2020-12-11 17:27:01 -0800

    added code to make 2019 and 2020 discharge measurement data consistent

--- inst/scripts/biomark_2019_2020/04_rollup_channel_unit.R
+++ inst/scripts/biomark_2019_2020/04_rollup_channel_unit.R
@@ -61,6 +61,43 @@ for(i in 1:length(otg_list)) {
 # clean up
 rm(otg_list)
 
+# make 2019 discharge measurement station_width's match 2020 format
+if(grepl("2019$", otg_path)) {
+  otg_all$discharge_measurements = otg_all$discharge_measurements %>%
+    group_by(parent_global_id) %>%
+    mutate(width_lag = lag(station_width),
+           station_width = if_else(is.na(width_lag),
+                                   station_width,
+                                   station_width - width_lag)) %>%
+    select(-width_lag)
+}
+
+# in 2020, add a measurement at the short bank
+if(grepl("2020$", otg_path)) {
+  otg_all$discharge_measurements = otg_all$discharge_measurements %>%
+    group_by(parent_global_id) %>%
+    # determine which is the first and last station at a site
+    mutate(station = 1:n()) %>%
+    group_split() %>%
+    map(.f = function(x) {
+      if(x$station_width[x$station == 1] > 0) {
+        y = x %>%
+          bind_rows(x %>%
+                      filter(station == 1) %>%
+                      mutate(object_id = NA,
+                             global_id = NA,
+                             station_width = 0,
+                             station_depth = 0,
+                             station_velocity = 0,
+                             station = 0)) %>%
+          arrange(station)
+        return(y)
+      } else {
+        return(x)
+      }
+    }) %>%
+    map_df(.f = identity)
+}
 #-------------------------
 # roll up all the OTG data to CU scale
 #-------------------------
