108a1f129537b6d6accc298eee2ec2a657ac1cfd
Kevin See <Kevin.See@merck.com>
2021-01-20 17:15:24 -0800

    added script to fix survey dates on 2_qcd_csv files. Can probably be deleted after it's run

---
+++ inst/scripts/biomark_2019_2020/E03_fix_qc_survey_dates.R
@@ -0,0 +1,101 @@
+# Author: Kevin See
+# Purpose: Fix survey dates on 2_qcd_csv files
+# Created: 1/20/21
+# Last Modified: 1/20/21
+# Notes:
+
+#-----------------------------------------------------------------
+# load needed libraries
+library(tidyverse)
+library(beepr)
+library(janitor)
+library(DASH)
+# devtools::load_all()
+
+#-------------------------
+# set NAS prefix, depending on operating system
+#-------------------------
+if(.Platform$OS.type != 'unix') {
+  nas_prefix = "S:"
+} else if(.Platform$OS.type == 'unix') {
+  nas_prefix = "~/../../Volumes/ABS"
+}
+
+#-----------------------------
+# set some arguments/parameters
+#-----------------------------
+# create a vector to directories containing the raw data, formatted for import
+yr_wtsd = c("2019/lemhi",
+            "2019/nf_salmon",
+            "2020/lemhi",
+            "2020/secesh")
+
+
+#-----------------------------
+# loop over year_watershed combinations
+#-----------------------------
+for (yw in yr_wtsd) {
+
+  # set the directories
+  path_format = paste0(nas_prefix,
+                       "/data/habitat/DASH/OTG/",
+                       yw,
+                       "/1_formatted_csvs/")
+
+  path_qcd = paste0(nas_prefix,
+                    "/data/habitat/DASH/OTG/",
+                    yw,
+                    "/2_qcd_csvs/")
+  # path_qcd = "~/Desktop/"
+
+  # read in the data from the 2_qcd_csv folder
+  otg_qcd = read_otg_csv_wrapper(path = path_qcd,
+                                 otg_type_list = c("surveyPoint_0.csv",
+                                                   "CU_1.csv",
+                                                   "Wood_2.csv",
+                                                   "Jam_3.csv",
+                                                   "Undercut_4.csv",
+                                                   "Discharge_5.csv",
+                                                   "DischargeMeasurements_6.csv"),
+                                 otg_type_names = c("survey",
+                                                    "cu",
+                                                    "wood",
+                                                    "jam",
+                                                    "undercut",
+                                                    "discharge",
+                                                    "discharge_measurements"))
+
+  # overwrite csvs with updated values
+  otg_qcd %>%
+    walk(.f = function(x) {
+      # fix issue with writing date from survey back to csv (grab date from 1_formatted_csv folder)
+      if("Survey Date" %in% names(x)) {
+        x = x %>%
+          mutate(`Survey Date` = map_chr(path_nm,
+                                         .f = function(y) {
+                                           suppressMessages(read_csv(paste0(path_format, y))) %>%
+                                             pull(`Survey Date`)
+                                         }))
+      }
+
+      if(sum(c("CreationDate", "EditDate") %in% names(x)) > 0) {
+        x = x %>%
+          mutate(across(c(CreationDate, EditDate),
+                        ~ format(., "%m/%d/%Y")))
+      }
+
+      x %>%
+        group_by(path_nm) %>%
+        group_split() %>%
+        map(.f = function(y) {
+          y %>%
+            select(-path_nm) %>%
+            write_csv(paste0(path_qcd, unique(y$path_nm)))
+          return(NULL)
+        })
+      return(NULL)
+    })
+
+  # after last loop, sound an alarm
+  if (yw == tail(yr_wtsd, 1)) { beepr::beep(3) }
+}
