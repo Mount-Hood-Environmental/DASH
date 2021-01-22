190e77e8df7ab61d70cebee2912cd4d8b165b67a
Mike Ackerman <dirtpathwanderer@gmail.com>
2021-01-22 16:34:48 -0700

    streamlined 01 script

--- inst/scripts/biomark_2019_2020/01_otg_import_qc.R
+++ inst/scripts/biomark_2019_2020/01_otg_import_qc.R
@@ -4,7 +4,7 @@
 # DASH data collected using Survey123.
 #
 # Created: July 15, 2020
-#   Last Modified: January 20, 2021
+#   Last Modified: January 22, 2021
 #
 # Notes:
 
@@ -29,16 +29,14 @@ if(.Platform$OS.type != 'unix') {
   nas_prefix = "~/../../Volumes/ABS"
 }
 
-
 #-----------------------------
 # Do you need to copy initial formatted files to a QC'd folder?
 #-----------------------------
 # if copy_to_qcd is set to TRUE, the files contained in the /1_formatted_csvs/
 # folder will be copied into a /2_qcd_csvs/ folder to have QC edits made.
 # If /2_qcd_csvs/ folders already exist, this will overwrite what's in there.
-# So please set to TRUE only the first time running this script
+# So please set to TRUE only the first time running this script!
 copy_to_qcd = FALSE
-# copy_to_qcd = TRUE
 
 #-----------------------------
 # set some arguments/parameters
@@ -50,7 +48,7 @@ yr_wtsd = c("2019/lemhi",
             "2020/secesh")
 
 #-----------------------------
-# import raw OTG data; loop over year_watershed combinations
+# LOOP 1: import raw OTG data; loop over year_watershed combinations
 #-----------------------------
 for (yw in yr_wtsd) {
 
@@ -77,10 +75,6 @@ for (yw in yr_wtsd) {
                                                     "discharge",
                                                     "discharge_measurements"))
 
-  # EXAMPLE: read in just one type of OTG data
-  # otg_raw_cu = read_otg_csv(path,
-  #                           otg_type = "CU_1.csv")
-
   # QC all data types for each yr_wtsd
   otg_raw$qc_results = qc_wrapper(survey_df = otg_raw$survey,
                                   cu_df = otg_raw$cu,
@@ -90,22 +84,6 @@ for (yw in yr_wtsd) {
                                   discharge_df = otg_raw$discharge,
                                   discharge_meas_df = otg_raw$discharge_measurements,
                                   redirect_output = F)
-
-  #-----------------------------
-  # ALTERNATIVE EXAMPLES: QC just a couple otg_types, separately
-  #-----------------------------
-  # qc_results_cu = qc_cu(qc_df = otg_raw$cu)
-  # qc_results_wood = qc_wood(qc_df = otg_raw$wood)
-  #
-  # qc_results_some = qc_results_cu %>%
-  #   tibble::add_column(source = "CU",
-  #                      .before = 0) %>%
-  #   bind_rows(qc_results_wood %>%
-  #               tibble::add_column(source = "Wood",
-  #                                  .before = 0))
-  #
-  # qc_results_some = qc_wrapper(cu_df = otg_raw$cu,
-  #                              wood_df = otg_raw$wood)
 
   #-----------------------------
   # if you need to copy the files into a QC'd series of folders
@@ -157,7 +135,7 @@ for (yw in yr_wtsd) {
       })
   }
 
-  # save the otg_raw list of dfs, and the initial QC messsages
+  # save the otg_raw list of dfs, and the initial QC flags
   save(otg_raw,
        file = paste0(nas_prefix,
                      "data/habitat/DASH/OTG/",
@@ -169,10 +147,83 @@ for (yw in yr_wtsd) {
 
 } # end import raw, QC, and save loop
 
-# END LOOP 1, START LOOP 2
+#-----------------------------
+# LOOP 2: iterative loop: import "QC" .csvs, run QC, write QC results,
+# fix problems in said "QC" .csvs, rinse and repeat...
+#-----------------------------
+for (yw in yr_wtsd) {
+
+  # set path for yr_wtsd, except QC'd data
+  path = paste0(nas_prefix,
+                "/data/habitat/DASH/OTG/",
+                yw,
+                "/2_qcd_csvs/")
+
+  otg_interim = read_otg_csv_wrapper(path = path,
+                                     otg_type_list = c("surveyPoint_0.csv",
+                                                       "CU_1.csv",
+                                                       "Wood_2.csv",
+                                                       "Jam_3.csv",
+                                                       "Undercut_4.csv",
+                                                       "Discharge_5.csv",
+                                                       "DischargeMeasurements_6.csv"),
+                                    otg_type_names = c("survey",
+                                                       "cu",
+                                                       "wood",
+                                                       "jam",
+                                                       "undercut",
+                                                       "discharge",
+                                                       "discharge_measurements"))
+
+  # perform QC on the qc'd data
+  qc_interim = qc_wrapper(survey_df = otg_interim$survey,
+                          cu_df = otg_interim$cu,
+                          wood_df = otg_interim$wood,
+                          jam_df = otg_interim$jam,
+                          undercut_df = otg_interim$undercut,
+                          discharge_df = otg_interim$discharge,
+                          discharge_meas_df = otg_interim$discharge_measurements,
+                          redirect_output = F)
+
+  # write interim QC results
+  write_csv(qc_interim, paste0(nas_prefix,
+                               "/data/habitat/DASH/OTG/",
+                               yw,
+                               "/2_qcd_csvs/qc_interim_",
+                               format(Sys.Date(), format = "%Y%m%d"),
+                               ".csv"))
+
+  # save the otg_interim list of dfs
+  save(otg_interim,
+       file = paste0(nas_prefix,
+                     "data/habitat/DASH/OTG/",
+                     yw,
+                     "/2_qcd_csvs/otg_interim.rda"))
+
+  # after last loop, sound an alarm
+  if (yw == tail(yr_wtsd, 1)) { beepr::beep(3) }
+
+} # end interim QC loop
+
+# IMPORTANT: THE ABOVE LOOP 1) IMPORTING QC'D DATA AND QC'ING THAT DATA IS A
+# POTENTIALLY ITERATIVE PROCESS, DESCRIBED BELOW
+
+# At this point, someone very familiar with the OTG data, (preferably a field technician or field coordinator,
+# secondarily a project leader) should intervene, review the QC errors flagged on the /2_qcd_csvs/ versions of
+# the data that we just wrote out, attempt to resolve those, and ideally, make notes for those QC errors that can't
+# be resolved. Errors should be resolved to the .csvs within the survey folders in the /2_qcd_csvs/ directory.
+
+# In addition, for the QC errors that are resolved, it is useful to provide notes on how they are resolved. Those
+# notes on how errors were/were not resolved care helpful towards improving potential data validation (i.e.,
+# validation of data during field surveys) or quality control steps in the future.
+
+# After resolving QC's to the best of your abilities, the next step is to re-run the above loop. This will
+# re-import the .csvs with the "improved" data and re-run the QC, exporting a new summary of remaining flags,
+# which can then be reviewed again. This process can be repeated until as many errors are resolved as possible.
+# It is fine to add notes in the above "qc_final_YYYYMMDD.csv" data about how errors were or were not resolved.
 
 #-----------------------------
-# import QC'd OTG data; loop over year_watershed combinations
+# LOOP 3: re-import "QC" .csvs, fix common mistakes w/ below code, overwrite .csvs
 #-----------------------------
 for (yw in yr_wtsd) {
 
@@ -271,29 +322,6 @@ for (yw in yr_wtsd) {
   # overwrite csvs with updated values
   otg_qcd %>%
     walk(.f = function(x) {
-      # fix issue with writing date from survey back to csv
-      if("Survey Date" %in% names(x)) {
-        x = x %>%
-          mutate(`Survey Date` = map_chr(path_nm,
-                                    .f = function(y) {
-                                      suppressMessages(read_csv(paste0(path, y))) %>%
-                                        pull(`Survey Date`)
-                                    }))
-      }
-
-      if("CreationDate" %in% names(x)) {
-        x = x %>%
-          mutate(CreationDate = map_chr(path_nm,
-                                        .f = function(y) {
-                                          suppressMessages(read_csv(paste0(path, y))) %>%
-                                            pull(CreationDate)
-                                        }),
-                 EditDate = map_chr(path_nm,
-                                    .f = function(y) {
-                                      suppressMessages(read_csv(paste0(path, y))) %>%
-                                        pull(EditDate)
-                                    }))
-      }
 
       x %>%
         group_by(path_nm) %>%
@@ -305,6 +333,24 @@ for (yw in yr_wtsd) {
         })
     })
 
+  # perform QC on the "final" OTG data
+  qc_final = qc_wrapper(survey_df = otg_qcd$survey,
+                        cu_df = otg_qcd$cu,
+                        wood_df = otg_qcd$wood,
+                        jam_df = otg_qcd$jam,
+                        undercut_df = otg_qcd$undercut,
+                        discharge_df = otg_qcd$discharge,
+                        discharge_meas_df = otg_qcd$discharge_measurements,
+                        redirect_output = F)
+
+  # write "final" QC results
+  write_csv(qc_final, paste0(nas_prefix,
+                             "/data/habitat/DASH/OTG/",
+                             yw,
+                             "/2_qcd_csvs/qc_final_",
+                             format(Sys.Date(), format = "%Y%m%d"),
+                             ".csv"))
+
   # save as .Rdata object
   save(otg_qcd,
        file = paste0(nas_prefix,
@@ -315,78 +361,6 @@ for (yw in yr_wtsd) {
   # after last loop, sound an alarm
   if (yw == tail(yr_wtsd, 1)) { beepr::beep(3) }
 
-} # end import QC'd data and save loop
-
-#-----------------------------
-# "Final" data QC
-#-----------------------------
-for (yw in yr_wtsd) {
-
-  # # load QC'd data
-  # load(paste0(nas_prefix,
-  #             "/data/habitat/DASH/OTG/",
-  #             yw,
-  #             "/prepped/otg_qcd.rda"))
-
-  # read in QC'd data from CSVs
-  otg_qcd = read_otg_csv_wrapper(path = path,
-                                 otg_type_list = c("surveyPoint_0.csv",
-                                                   "CU_1.csv",
-                                                   "Wood_2.csv",
-                                                   "Jam_3.csv",
-                                                   "Undercut_4.csv",
-                                                   "Discharge_5.csv",
-                                                   "DischargeMeasurements_6.csv"),
-                                 otg_type_names = c("survey",
-                                                    "cu",
-                                                    "wood",
-                                                    "jam",
-                                                    "undercut",
-                                                    "discharge",
-                                                    "discharge_measurements"))
-
-  # perform QC on the qc'd data
-  qc_final = qc_wrapper(survey_df = otg_qcd$survey,
-                        cu_df = otg_qcd$cu,
-                        wood_df = otg_qcd$wood,
-                        jam_df = otg_qcd$jam,
-                        undercut_df = otg_qcd$undercut,
-                        discharge_df = otg_qcd$discharge,
-                        discharge_meas_df = otg_qcd$discharge_measurements,
-                        redirect_output = F)
-
-  qc_final_path = paste0(nas_prefix,
-                         "/data/habitat/DASH/OTG/",
-                         yw,
-                         "/2_qcd_csvs/qc_final")
-  save(qc_final, file = paste0(qc_final_path, ".rds"))
-  write_csv(qc_final, paste0(qc_final_path,
-                             "_",
-                             format(Sys.Date(), format = "%Y%m%d"),
-                             ".csv"))
-
-} # end qc final data loop
-
-# IMPORTANT: THE ABOVE TWO LOOPS 1) IMPORTING QC'D DATA AND QC'ING THAT DATA IS A
-# POTENTIALLY ITERATIVE PROCESS, DESCRIBED BELOW
-
-# At this point, someone very familiar with the OTG data, (preferably a field technician or field coordinator,
-# secondarily a project leader) should intervene, review the QC errors flagged on the /2_qcd_csvs/ versions of
-# the data that we just wrote out, attempt to resolve those, and ideally, make notes for those QC errors that can't
-# be resolved. Errors should be resolved to the .csvs within the survey folders in the /2_qcd_csvs/ directory.
-
-# In addition, for the QC errors that are resolved, it is useful to provide notes on how they are resolved. Those
-# notes on how errors were/were not resolved care helpful towards improving potential data validation (i.e.,
-# validation of data during field surveys) or quality control steps in the future.
-
-# After resolving QC's to the best of your abilities, the next step is to re-run the above loop. This will
-# re-import the .csvs with the "improved" data and re-run the QC, exporting a new summary of remaining flags,
-# which can then be reviewed again. This process can be repeated until as many errors are resolved as possible.
-# It is fine to add notes in the above "qc_final_YYYYMMDD.csv" data about how errors were or were not resolved.
-
-# Now that errors have been resolved (which as on 20210120 they all have not, still need to review), we can proceed.
-# As of 20210120, the majority of QC flags in the "2019/lemhi" data have been reviewed, but those in the
-# "2019/nf_salmon", "2020/lemhi", and "2020/secesh" have not.
+} # end import QC'd data, apply automatic fixes, and export prepped data
 
 # END SCRIPT
-
