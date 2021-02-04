66b4e8ce4d21de14b9bbf243aed3a0449b386e4f
Mike Ackerman <dirtpathwanderer@gmail.com>
2021-02-04 16:48:08 -0700

    edits to 02 script

--- inst/scripts/biomark_2019_2020/02_rollup_channel_unit.R
+++ inst/scripts/biomark_2019_2020/02_rollup_channel_unit.R
@@ -1,9 +1,9 @@
 # Authors: Kevin See & Mike Ackerman
 #
-# Purpose: rollup all OTG data to channel unit scale
+# Purpose: rollup OTG data to channel unit scale
 #
 # Created: December 9, 2020
-# Last Modified: January 20, 2021
+# Last Modified: February 4, 2021
 #
 # Notes:
 
@@ -13,9 +13,7 @@ rm(list = ls())
 #-----------------------------
 # load necessary libraries
 #-----------------------------
-library(dplyr)
-library(purrr)
-library(stringr)
+library(tidyverse)
 library(janitor)
 library(sf)
 library(DASH)
@@ -33,55 +31,57 @@ if(.Platform$OS.type != 'unix') {
 # load QC'd OTG data
 #-------------------------
 otg_path = paste0(nas_prefix,
-                  "/data/habitat/DASH/OTG/")
+                  "/data/habitat/DASH/OTG")
 
 # list of otg_qcd files in otg_path
 # take advantage of fact that 2018 data doesn't contain a "^otg_qcd.rda$"
-otg_list = list.files(path = otg_path,
-                      pattern = "^otg_qcd.rda$",
-                      recursive = T) %>%
+otg_qcd_paths = list.files(path = otg_path,
+                           pattern = "^otg_qcd.rda$",
+                           recursive = T) %>%
   paste(otg_path, ., sep = "/") %>%
-  as.list() %>%
+  as.list()
+
+# load each of otg_qcd_paths
+otg_list = otg_qcd_paths %>%
   map(.f = function(x) {
     load(x) %>%
       get() %>%
       map(clean_names)
   })
 
-# combine elements of otg_list into otg_all
+# combine elements of otg_list into otg
 for(i in 1:length(otg_list)) {
   if(i == 1) {
-    otg_all = otg_list[[1]]
+    otg = otg_list[[1]]
   } else {
-    otg_all = suppressMessages(purrr::map2(otg_all,
-                                           otg_list[[i]],
-                                           dplyr::full_join))
+    otg = suppressMessages(purrr::map2(otg,
+                                       otg_list[[i]],
+                                       dplyr::full_join))
   }
 }
 
 # clean up
-rm(otg_list)
+rm(otg_qcd_paths, otg_list)
 
-
-# clean site names
-otg_all$survey = otg_all$survey %>%
+# clean site_names
+otg$survey = otg$survey %>%
   mutate(across(site_name,
-                ~ str_replace(., "_.*", "")),
+                ~str_replace(., "_.*", "")),
          across(site_name,
-                ~str_remove(., " ")))
-tabyl(otg_all$survey,
+                ~str_remove_all(., " ")))
+tabyl(otg$survey,
       site_name)
 
 #-------------------------
 # roll up all the OTG data to CU scale
 #-------------------------
-cu_df = otg_to_cu(otg_all$survey,
-                  otg_all$cu,
-                  otg_all$jam,
-                  otg_all$undercut,
-                  otg_all$wood,
-                  otg_all$discharge,
-                  otg_all$discharge_measurements)
+cu_df = otg_to_cu(otg$survey,
+                  otg$cu,
+                  otg$jam,
+                  otg$undercut,
+                  otg$wood,
+                  otg$discharge,
+                  otg$discharge_measurements)
 
 # save as prepped data
 write_csv(cu_df,
@@ -95,28 +95,28 @@ write_rds(cu_df,
 #-------------------------
 # QC the rollup
 #-------------------------
-qc_roll = qc_rollup(otg_all$survey,
-                    otg_all$cu,
-                    otg_all$jam,
-                    otg_all$undercut,
-                    otg_all$wood,
-                    otg_all$discharge,
-                    otg_all$discharge_measurements)
+qc_roll = qc_rollup(otg$survey,
+                    otg$cu,
+                    otg$jam,
+                    otg$undercut,
+                    otg$wood,
+                    otg$discharge,
+                    otg$discharge_measurements)
 
 qc_roll$error_df
 
 # what do the individual data look like for these dangling channel units?
 qc_roll$miss_rollup %>%
   filter(source == "Wood") %>%
-  left_join(otg_all$wood)
+  left_join(otg$wood)
 
 qc_roll$miss_rollup %>%
   filter(source == "Jam") %>%
-  left_join(otg_all$jam)
+  left_join(otg$jam)
 
 qc_roll$miss_rollup %>%
   filter(source == "Undercut") %>%
-  left_join(otg_all$undercut)
+  left_join(otg$undercut)
 
 # where are the dangling discharge measurements?
 qc_roll$miss_discharge
