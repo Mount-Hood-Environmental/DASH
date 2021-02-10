6bd903fb1a9b60dc548ff9f68aad40170d75641a
Mike Ackerman <dirtpathwanderer@gmail.com>
2021-02-10 14:49:27 -0700

    forgot to add an importFrom

--- R/otg_to_cu.R
+++ R/otg_to_cu.R
@@ -14,6 +14,7 @@
 #' @import dplyr
 #' @importFrom magrittr %<>%
 #' @importFrom janitor clean_names
+#' @importFrom tidyr replace_na
 #' @export
 #' @return a data.frame summarizing data for channel units
 
