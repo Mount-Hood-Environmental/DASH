630c32a2357f0408f992233a90ac496c2a3795a4
Mike Ackerman <dirtpathwanderer@gmail.com>
2022-03-16 14:23:23 -0700

    updated rollup_cu_discharge()

--- R/rollup_cu_discharge.R
+++ R/rollup_cu_discharge.R
@@ -1,7 +1,7 @@
 #' @title Rollup Channel Unit Discharge Data
 #'
-#' @description Summarize discharge data (e.g., from `otg_type =` "Discharge_5.csv" and
-#' "DischargeMeasurements_6.csv") for channel units where discharge was measured
+#' @description Summarize discharge data (e.g., from `otg_type =` "Discharge_5.csv") for
+#' channel units where discharge was measured
 #'
 #' @author Mike Ackerman, Kevin See, and Richie Carmichael
 #'
@@ -13,20 +13,11 @@
 #' @export
 #' @return a data.frame summarizing discharge measurements at the channel unit scale
 
-rollup_cu_discharge = function(discharge_df = NULL,
-                               discharge_meas_df = NULL) {
+rollup_cu_discharge = function(discharge_df = NULL) {
 
   stopifnot(!is.null(discharge_df))
-  stopifnot(!is.null(discharge_meas_df))
 
-  disch_tmp = calc_discharge(discharge_meas_df = discharge_meas_df)
-
-  return_df = discharge_df %>%
-    dplyr::select(global_id,
-                  parent_global_id,
-                  discharge_location_bos_tos_cu_number) %>%
-    dplyr::left_join(disch_tmp,
-                     by = c("global_id" = "parent_global_id"))
+  return_df = calc_discharge(discharge_df = discharge_df)
 
   return(return_df)
 
