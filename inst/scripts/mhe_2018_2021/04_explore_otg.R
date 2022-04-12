f92ee3264dc0cd8c72fb69f27087747c5fcdb765
Mike Ackerman <dirtpathwanderer@gmail.com>
2022-04-12 08:46:51 -0600

    initiating 04 script

---
+++ inst/scripts/mhe_2018_2021/04_explore_otg.R
@@ -0,0 +1,17 @@
+# Authors: Mike Ackerman
+#
+# Purpose: Start to explore the complete OTG dataset and begin
+# to roll up measurements into habitat reach metrics
+#
+# Initially created: April 12, 2022
+#   Last Modified:
+#
+# Notes:
+
+# clear environment
+rm(list = ls())
+
+#-----------------------------
+# load necessary libraries
+#-----------------------------
+
