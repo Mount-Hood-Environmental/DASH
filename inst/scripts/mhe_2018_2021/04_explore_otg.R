b6ecdd975d36ee3a17c97ad12e9986e59695d55a
Mike Ackerman <dirtpathwanderer@gmail.com>
2022-04-21 15:05:07 -0600

    progress on metric generation

--- inst/scripts/mhe_2018_2021/04_explore_otg.R
+++ inst/scripts/mhe_2018_2021/04_explore_otg.R
@@ -4,7 +4,7 @@
 # to roll up measurements into habitat reach metrics
 #
 # Initially created: April 12, 2022
-#   Last Modified:
+#   Last Modified: April 21, 2022
 #
 # Notes:
 
@@ -48,15 +48,17 @@ st_write(cu_sf,
          dsn = paste0(nas_prefix, "main/data/habitat/DASH/prepped/dash_cu_18-21.gpkg"),
          delete_dsn = T)
 
+tmp = paste(list(cu_sf$cu_id), sep = "-")
+
 #-------------------------
 # initiate habitat reach sf
 #-------------------------
 hr_sf = cu_sf %>%
   group_by(site_name, year, hab_rch) %>%
   summarise(
-    hr_length_m = sum(cu_length_m),
+    hr_length_m = round(sum(cu_length_m), 1),
     # channel unit counts
-    cu_ids = list(unique(cu_id)),
+    #cu_ids = list(unique(cu_id)),
     n_cu = n_distinct(cu_id),
     n_seg = n_distinct(channel_segment_number),
     n_pool = sum(channel_unit_type == "Pool"),
@@ -93,29 +95,62 @@ hr_sf = cu_sf %>%
     fish_cov_art = round(weighted.mean(artificial_percent, cu_length_m), 1),
     fish_cov_total = sum(fish_cov_tr_veg, fish_cov_aq_veg, fish_cov_lwd, fish_cov_art),
     fish_cov_none = round(weighted.mean(total_no_cover_percent, cu_length_m), 1),
-    # size
-    hr_max_depth_m = round(max(maximum_depth_m, na.rm = T), 1),
     # substrate ocular estimates
     sub_est_sand_fines = round(weighted.mean(sand_fines_2mm_percent, cu_length_m), 1),
     sub_est_gravl = round(weighted.mean(gravel_2_64mm_percent, cu_length_m), 1),
     sub_est_cbl = round(weighted.mean(cobble_64_256mm_percent, cu_length_m), 1),
     sub_est_bldr = round(weighted.mean(boulder_256mm_percent, cu_length_m), 1),
     # diameter estimates from pebble counts
-    hr_d16_mm = mean(cu_d16, na.rm = T),
-    hr_d50_mm = mean(cu_d50, na.rm = T),
-    hr_d84_mm = mean(cu_d84, na.rm = T),
+    hr_d16_mm = round(mean(cu_d16, na.rm = T), 1),
+    hr_d50_mm = round(mean(cu_d50, na.rm = T), 1),
+    hr_d84_mm = round(mean(cu_d84, na.rm = T), 1),
     # wood
-
+    n_lwd_bankfull = sum(lwd_n),
+    n_lwd_wetted = sum(lwd_n_wet),
+    lwd_bankfull_area_m2 = sum(lwd_area_m2),
+    lwd_wetted_area_m2 = sum(lwd_area_wet_m2),
+    lwd_bankfull_vol_m3 = sum(lwd_vol_m3),
+    lwd_wetted_vol_m3 = sum(lwd_vol_wet_m3),
+    lwd_bankfull_slow_vol_m3 = sum(lwd_vol_m3[channel_unit_type == "Pool"], lwd_vol_m3[channel_unit_type == "OCA"]),
+    lwd_wetted_slow_vol_m3 = sum(lwd_vol_wet_m3[channel_unit_type == "Pool"], lwd_vol_wet_m3[channel_unit_type == "OCA"]),
+    # jams
+    n_jam = sum(jam_n),
+    n_jam_pieces = sum(jam_est_n_pieces),
+    jam_area_m2 = sum(jam_area_m2),
+    jam_vol_m3 = sum(jam_vol_m3),
+    # all wood
+    n_lwd_all = sum(n_lwd_bankfull, n_jam_pieces),
+    wood_all_area_m2 = sum(lwd_bankfull_area_m2, jam_area_m2),
+    wood_all_vol_m3 = sum(lwd_bankfull_vol_m3, jam_vol_m3),
     # undercuts
-
+    undct_length_m = sum(undct_length_m),
+    undct_area_m2 = sum(undct_area_m2),
+    undct_perc = round(undct_length_m / hr_length_m * 100, 1),
+    # complexity
+    hr_ms_sin_cl = round(weighted.mean(cu_sin_cl[channel_segment_number == "01"], cu_length_m[channel_segment_number == "01"], na.rm = T), 2),
+    hr_sin_cl = round(weighted.mean(cu_sin_cl, cu_length_m, na.rm = T), 2),
+    hr_thlwg_dpth_cv = round(raster::cv(thalweg_exit_depth_m, na.rm = T), 2),
+    hr_braidedness = round(sum(cu_length_m) / sum(cu_strght_m[channel_segment_number == "01"]), 2),
+    # size
+    hr_thlwg_dpth_avg_m = round(mean(thalweg_exit_depth_m, na.rm = T), 2),
+    hr_max_depth_m = round(max(maximum_depth_m, na.rm = T), 2),
     # temperature
     obs_water_temp_c = unique(site_water_temp_c),
     # water quality
     obs_conductivity_ms = unique(site_conductivity_ms),
     .groups = "drop"
-  )
+  ) %>%
+  rename(geometry = geom) #%>%
+  #st_cast("MULTILINESTRING")
 
 # write habitat reach sf object to file
 saveRDS(hr_sf,
         file = paste0(nas_prefix, "main/data/habitat/DASH/prepped/dash_hr_18-21.rds"))
 
+write_csv(hr_sf,
+          file = paste0(nas_prefix, "main/data/habitat/DASH/prepped/dash_hr_18-21.csv"))
+
+# write habitat reach sf as geodatabase
+# st_write(hr_sf,
+#          dsn = paste0(nas_prefix, "main/data/habitat/DASH/prepped/dash_hr_18-21.gpkg"),
+#          delete_dsn = T)
